0e4d2b00893d616dd3817096209482591ae4d3be
emmakamarainen <emmakamarainen@users.noreply.github.com>
2015-03-19 23:32:12 +0200

    lisätty avain

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,7 +2,8 @@
 create table Kayttaja(
 id serial primary key,
 nimimerkki varchar(50) not null,
-salasana varchar(50) not null
+salasana varchar(50) not null,
+yllapitaja boolean
 );
 
 create table Juoma(
@@ -11,7 +12,7 @@ kayttaja_id integer references Kayttaja(id),
 nimi varchar(50) not null,
 lisayspvm DATE not null,
 juomalaji varchar(50) not null,
-/*ainesosat foreign key TSEKKAA*/
+ainesosat_id integer foreign key references Ainesosat(ainesosat_id)
 kuvaus varchar(400)
 );
 
