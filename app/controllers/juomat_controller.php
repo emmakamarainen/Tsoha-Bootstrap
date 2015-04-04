51648462c36dcbc2d1db18a3a8394cc5cf495a9f
emmakamarainen <emmakamarainen@users.noreply.github.com>
2015-04-04 18:01:29 +0300

    muokkaus- ja poistometodit

--- app/controllers/juomat_controller.php
+++ app/controllers/juomat_controller.php
@@ -57,5 +57,42 @@ class JuomatController extends BaseController {
         $aineet = Ainesosa::all();
         View::make('aine/aine_list.html', array('aineet' => $aineet));
     }
+    
+    public static function edit($id) {
+        $juoma = Juoma::find($id);
+        View::make('drink/edit.html', array('attributes' => $juoma));
+    }
+    
+    public static function update($id){
+    $params = $_POST;
 
+    $attributes = array(
+        'id' => $id,
+        'nimi' => $params['nimi'],
+        'lisayspvm' => $params['lisayspvm'],
+        'kayttaja_id' => $params['kayttaja_id'],
+        'ainesosat' => $params['ainesosat'],
+        'juomalaji' => $params['juomalaji'],
+        'kuvaus' => $params['kuvaus'],
+    );
+    
+    // Alustus
+    $juoma = new Juoma($attributes);
+    $errors = $juoma->errors();
+
+    if(count($errors) > 0){
+      View::make('drink/edit.html', array('errors' => $errors, 'attributes' => $attributes));
+    }else{
+      $juoma->update();
+
+      Redirect::to('/drink/' . $juoma->id, array('message' => 'Muokkaus onnistui.'));
+      }
+    }
+    
+    public static function destroy($id){
+    $juoma = new Juoma(array('id' => $id));
+    $juoma->destroy();
+    Redirect::to('/drink', array('message' => 'Poistettu.'));
+  }
+    
 }
